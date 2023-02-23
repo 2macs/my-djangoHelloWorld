e20d84f87729e833ceefb57e7e0a9ba7ff192da2
2macs <adrian.mcneill@outlook.com>
2023-02-23 12:51:38 +0000

    render pages

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Analagous to a DB Table
+# NULL = FALSE MEANS ANY ITEM WITHOUT A NAME WILL NOT BE CREATED
+# Blank = False makes the field a required field
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+
+# Overides defualt name from inherited models class, changes it to actual name
+
+    def __str__(self):
+        return self.name
