bfc2e58f00867f37bff2889fab55f7cdf6a3123d
2macs <adrian.mcneill@outlook.com>
2023-02-24 13:23:52 +0000

    add model tests

---
+++ todo/test_views.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from .models import Item
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')   # get the home page
+        self.assertAlmostEqual(response.status_code, 200)  # 200 indicates page opened successfully
+        self.assertTemplateUsed(response, 'todo/todo_list.html')  # correct template is used
+    
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')  
+        self.assertAlmostEqual(response.status_code, 200)  
+        self.assertTemplateUsed(response, 'todo/add_item.html')  
+
+    def test_get_edit_item_page(self):
+        # create an item and get its ID
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')  
+        self.assertAlmostEqual(response.status_code, 200)  
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
+
+    def test_can_add_item(self):
+        response = self.client.post('/add', {'name': 'Test added item'})
+        self.assertRedirects(response, '/')
+
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/delete/{item.id}')
+        self.assertRedirects(response, '/')
+        # check no repord in DBase after delete
+        existing_items = Item.objects.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
+        
+
+    def test_can_toggle_item(self):
+        item = Item.objects.create(name='Test Todo Item', done=True)
+        response = self.client.get(f'/toggle/{item.id}')
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertFalse(updated_item.done)
